97404b0497b773c6ba38483969a8e07b57bca25c
getlarge <ed@getlarge.eu>
2021-06-17 10:55:25 +0200

    fix: add prettier rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
   ignorePatterns: ['**/*.min.js'],
   extends: [
     'eslint:recommended',
+    'prettier',
     'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
 };
