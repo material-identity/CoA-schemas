8b9d8acd93cea51713f0f8aa759f4b54fbd30ecb
getlarge <ed@getlarge.eu>
2022-02-21 07:07:29 +0100

    fix: remove schema version update

--- utils/generate-pdf-cli.js
+++ utils/generate-pdf-cli.js
@@ -6,7 +6,6 @@ const { version: pkgVersion } = require('../package.json');
   const certificatePattern = argv[2] || 'test/fixtures/valid_certificate_*.json';
   try {
     const updater = new SchemaRepositoryVersion(defaultServerUrl, [], pkgVersion, translations, 'schema.json');
-    await updater.updateSchemasVersion();
     await updater.updatePdfFixturesVersion(certificatePattern, pdfGeneratorPath, pdfDocDefinition(), pdfFonts);
   } catch (error) {
     console.error(error.message);
