4ce54607cb11457d1e9e41b24fb251baed586bdb
getlarge <ed@getlarge.eu>
2022-03-23 14:50:02 +0100

    feat: allow extra translations for PDF generation

--- utils/generate-pdf-cli.js
+++ utils/generate-pdf-cli.js
@@ -1,13 +1,27 @@
 const { SchemaRepositoryVersion } = require('@s1seven/schema-tools-versioning');
-const { defaultServerUrl, pdfDocDefinition, pdfFonts, pdfGeneratorPath, translations } = require('./constants');
+const {
+  defaultServerUrl,
+  extraTranslations,
+  pdfDocDefinition,
+  pdfFonts,
+  pdfGeneratorPath,
+  translations,
+} = require('./constants');
 const { version: pkgVersion } = require('../package.json');
 
 (async function (argv) {
   const certificatePattern = argv[2] || 'test/fixtures/valid_certificate_*.json';
   try {
-    const updater = new SchemaRepositoryVersion(defaultServerUrl, [], pkgVersion, translations, 'schema.json');
+    const updater = new SchemaRepositoryVersion(
+      defaultServerUrl,
+      [],
+      pkgVersion,
+      translations,
+      extraTranslations,
+      'schema.json',
+    );
     await updater.updatePdfFixturesVersion(certificatePattern, pdfGeneratorPath, pdfDocDefinition(), pdfFonts);
   } catch (error) {
-    console.error(error.message);
+    console.error(error);
   }
 })(process.argv);
